68a61e295f7dffde6bb1c0e1bea0f347fa984291
liumeng <378338627@qq.com>
2016-10-10 09:04:55 +0800

    10100904
    测试用客户端程序

---
+++ www/js/controller.js
@@ -0,0 +1,28 @@
+/**
+ * Created by liumeng on 2016/9/14.
+ */
+angular.module('mainController',[])
+  .controller('MainCtrl',['$scope','$state',function($scope,$state){
+    $scope.$on('$ionicView.afterEnter',function(){
+      //alert(123);
+      iosocket.on('connect',function(){
+        console.log('连接成功啦');
+      });
+    });
+    $scope.message={
+
+    }
+    $scope.send=function(){
+      //iosocket.emit('private message', $stateParams.from._id, $stateParams.to._id, $scope.sendMessage);
+    }
+    $scope.$watch('message.messageDreamworker',function(newvalue,oldvalue){
+      //alert(newvalue+'和'+oldvalue);
+    });
+    $scope.WsendToDreams=function(){
+      //alert($scope.message.messageDreamworker);
+      iosocket.emit('private message', '57979dddcd8a42f1cc84690b', '577bc1ffca1d345501cf73b2', $scope.message.messageDreamworker);
+    }
+    $scope.MsendToDreams=function(){
+      iosocket.emit('private message', '5794d04de7957f7d9aee21d6', '577bc1ffca1d345501cf73b2', $scope.message.messageMetrix);
+    }
+  }])
